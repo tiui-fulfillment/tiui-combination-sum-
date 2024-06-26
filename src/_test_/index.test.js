916059326c5fb63fbecd2c15ec41bfa805c4d6cb
Nestor Jesus <nesjesramrey@gmail.com>
2024-06-26 14:30:20 -0600

    finish test

--- src/_test_/index.test.js
+++ src/_test_/index.test.js
@@ -1,15 +1,10 @@
-const combinationSum = require ('../index');
+const combinationSum = require("../index");
 
-describe('combinationSum', () => {
-  it('Debería encontrar todas las combinaciones con suma específica', () => {
-    expect(combinationSum([1], 4)).toEqual([
-      [1, 1, 1, 1],
-    ]);
+describe("combinationSum", () => {
+  it("Debería encontrar todas las combinaciones con suma específica", () => {
+    expect(combinationSum([1], 4)).toEqual([[1, 1, 1, 1]]);
 
-    expect(combinationSum([2, 3, 6, 7], 7)).toEqual([
-      [2, 2, 3],
-      [7],
-    ]);
+    expect(combinationSum([2, 3, 6, 7], 7)).toEqual([[2, 2, 3], [7]]);
 
     expect(combinationSum([2, 3, 5], 8)).toEqual([
       [2, 2, 2, 2],
