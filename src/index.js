4d77c238c155eaed02aad769964f9ad9c7e0504b
Jordan Higuera Higuera <jordandark123@gmail.com>
2024-06-26 13:28:54 -0700

    chore: comentarios

--- src/index.js
+++ src/index.js
@@ -8,17 +8,21 @@
  */
 
 const combinationSumRecursive = (candidates, remainingSum, finalCombinations = [], currentCombination = [], startFrom = 0) => {
+  // Caso base, si la suma restante es 0, entonces agregamos la combinación actual a las combinaciones finales.
   if (remainingSum === 0) {
     finalCombinations.push([...currentCombination]);
     return finalCombinations
   }
 
+  // Caso base, si la suma restante es negativa, o si hemos explorado todos los candidatos, o si el candidato actual es mayor que la suma restante, entonces regresamos.
   if (remainingSum < 0 || startFrom === candidates.length || remainingSum < candidates[startFrom]) {
     return finalCombinations;
   }
 
   currentCombination.push(candidates[startFrom]);
 
+  // Llamamos recursivamente a la función con la suma restante reducida por el candidato actual, si la suma restante es 0, entonces agregamos la combinación actual a las combinaciones finales.
+  // Si la suma restante es negativa, o si hemos explorado todos los candidatos, o si el candidato actual es mayor que la suma restante, entonces regresamos. con el dado caso anterior
   combinationSumRecursive(
     candidates,
     remainingSum - candidates[startFrom],
@@ -27,8 +31,10 @@ const combinationSumRecursive = (candidates, remainingSum, finalCombinations = [
     startFrom
   );
 
+  // Eliminamos el último elemento de la combinación actual para explorar otras posibilidades.
   currentCombination.pop();
 
+  // Llamamos recursivamente a la función con el siguiente candidato.
   combinationSumRecursive(
     candidates,
     remainingSum,
