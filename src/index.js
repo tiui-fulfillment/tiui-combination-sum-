6ef783d5951c042c027e7f3955ce8f6ab0c91848
Mauricio <mauricohh71@gmail.com>
2024-06-26 12:44:04 -0600

    Ejercicio Completado Combination sum

--- src/index.js
+++ src/index.js
@@ -7,9 +7,23 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+const combinationSumRecursive = (candidates, remainingSum, finalCombinations, currentCombination, startFrom) => {
+  // Si la suma restante es negativa, no se puede encontrar una combinación válida.
+  if (remainingSum < 0) return; 
+  // Si la suma restante es cero, se ha encontrado una combinación válida.
+  if (remainingSum === 0) {
+      finalCombinations.push([...currentCombination]); // Agregar una copia de la combinación actual a las combinaciones finales.
+      return;
   }
+  
+  // Se itera sobre los candidatos comenzando desde el indice startFrom para evitar combinaciones duplicadas.
+  for (let i = startFrom; i < candidates.length; i++) {
+      currentCombination.push(candidates[i]); // Agregar el candidato actual a la combinación actual.
+      //se llama recursivamente a la funcion con la suma restante actualizada y el mismo índice para permitir reutilización.
+      combinationSumRecursive(candidates, remainingSum - candidates[i], finalCombinations, currentCombination, i); 
+      currentCombination.pop(); // Remover el último candidato añadido para retroceder y explorar otras combinaciones.
+  }
+}
   
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
@@ -19,7 +33,9 @@ const combinationSumRecursive = (
    * @return {number[][]}
    */
 const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
+    const finalCombinations = [];
+    combinationSumRecursive(candidates, target, finalCombinations, [], 0);
+    return finalCombinations;
   }
 
 module.exports = combinationSum;
