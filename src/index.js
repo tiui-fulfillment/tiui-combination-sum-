83eed689282f2fc64de84bf37254c761597d6346
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2024-06-26 17:14:45 -0600

    feat: add function that finds all unique combinations

--- src/index.js
+++ src/index.js
@@ -6,20 +6,44 @@
  * @param {number} startFrom - index of the candidate to start further exploration from.
  * @return {number[][]}
  */
+const combinationSumRecursive = (candidate, remainingSum, finalCombinations = [], currentCombination = [], startFrom = 0) => {
+  // If remaining sum is 0, then currentCombination is a valid combination.
+  if (remainingSum === 0) {
+    finalCombinations.push(currentCombination.slice()); // make a copy of currentCombination to add to the finalCombinations.
+    return; // stop the exploration.
+  }
 
-const combinationSumRecursive = (
-    
+  // If remaining sum is negative, then the currentCombination is not valid.
+  if (remainingSum < 0) {
+    return; // currentCombination is not valid - stop the exploration.
   }
-  
-  /**
-   * Backtracking algorithm of finding all possible combination for specific sum.
-   *
-   * @param {number[]} candidates
-   * @param {number} target
-   * @return {number[][]}
-   */
+
+  // Try adding new candidates to currentCombination starting from startFrom.
+  for (let candidateIndex = startFrom; candidateIndex < candidate.length; candidateIndex += 1) {
+    const currentCandidate = candidate[candidateIndex];
+
+    // Add current candidate to the currentCombination.
+    currentCombination.push(currentCandidate);
+
+    // Try adding new candidates with the sum = remainingSum - currentCandidate.
+    combinationSumRecursive(candidate, remainingSum - currentCandidate, finalCombinations, currentCombination, candidateIndex);
+
+    // Backtrack: remove current candidate from the current combination.
+    currentCombination.pop();
+  }
+
+  return finalCombinations;
+}
+
+/**
+ * Backtracking algorithm of finding all possible combination for specific sum.
+ *
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
 const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
-  }
+  return combinationSumRecursive(candidates, target);
+}
 
 module.exports = combinationSum;
