dcfc9daedc41314045f326e987525444c90d969e
Jordan Higuera Higuera <jordandark123@gmail.com>
2024-06-26 13:15:43 -0700

    feat: combinationSumRecursive func

--- src/index.js
+++ src/index.js
@@ -7,10 +7,39 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+const combinationSumRecursive = (candidates, remainingSum, finalCombinations = [], currentCombination = [], startFrom = 0) => {
+  if (remainingSum === 0) {
+    finalCombinations.push([...currentCombination]);
+    return finalCombinations
   }
-  
+
+  if (remainingSum < 0 || startFrom === candidates.length || remainingSum < candidates[startFrom]) {
+    return finalCombinations;
+  }
+
+  currentCombination.push(candidates[startFrom]);
+
+  combinationSumRecursive(
+    candidates,
+    remainingSum - candidates[startFrom],
+    finalCombinations,
+    currentCombination,
+    startFrom
+  );
+
+  currentCombination.pop();
+
+  combinationSumRecursive(
+    candidates,
+    remainingSum,
+    finalCombinations,
+    currentCombination,
+    startFrom + 1
+  );
+
+  return finalCombinations;
+}
+
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
    *
