0daa7b28c532d2485c1089884aeff08e850d57b9
Jesus Mendoza <jesus.menlo2002@gmail.com>
2024-06-26 17:24:27 -0700

    Initial commit with changes from downloaded ZIP

---
+++ PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,13 @@
+## DESCRIPTION
+
+Nombre: Jesus Antonio Mendoza Loeza
+Email: jesus.menlo2002@gmail.com
+WhatsApp: 9994767608
+Ciudad: Merida
+Pais: Mexico
+
+## ¿Como explicarias el proposito de este proyecto? (opcional)
+Realizar una evaluacion a los postulantes de la vacantes, para evaluar sus skills y su capacidad de resolucion de problemas en un ejemplo como este, implementando un algoritmo de backtracking.
+
+## ¿Cuál es tu stack tecnológico preferido? ¿Por qué? (opcional)
+El MERN stack, su popularidad y soporte comunitario lo convierten en una excelente opción para muchos proyectos
---
+++ src/index.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} candidates - candidate numbers we're picking from.
+ * @param {number} remainingSum - remaining sum after adding candidates to currentCombination.
+ * @param {number[][]} finalCombinations - resulting list of combinations.
+ * @param {number[]} currentCombination - currently explored candidates.
+ * @param {number} startFrom - index of the candidate to start further exploration from.
+ * @return {number[][]}
+ */
+
+const combinationSumRecursive = (candidates, remainingSum, finalCombinations, currentCombination, startFrom) => {
+  if (remainingSum === 0) {
+      finalCombinations.push(Array.from(currentCombination));
+      return;
+  }
+
+  for (let i = startFrom; i < candidates.length; i++) {
+      if (candidates[i] > remainingSum) {
+          break;
+      }
+
+      currentCombination.push(candidates[i]);
+      combinationSumRecursive(candidates, remainingSum - candidates[i], finalCombinations, currentCombination, i);
+      currentCombination.pop();
+  }
+}
+  /**
+   * Backtracking algorithm of finding all possible combination for specific sum.
+   *
+   * @param {number[]} candidates
+   * @param {number} target
+   * @return {number[][]}
+   */
+const combinationSum = (candidates, target) => {
+  const finalCombinations = [];
+  candidates.sort((a, b) => a - b);
+  combinationSumRecursive(candidates, target, finalCombinations, [], 0);
+  return finalCombinations;
+  }
+
+module.exports = combinationSum;
