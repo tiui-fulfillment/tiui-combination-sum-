c34ff77e1556a1c0a1fc5d8eec5f6e76a537c0bd
Rafax-boop <rafaelminon@outlook.com>
2024-06-28 12:19:17 -0600

    Prueba resuelta

--- src/index.js
+++ src/index.js
@@ -7,8 +7,32 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+const combinationSumRecursive = (candidates, remainingSum, finalCombinations, currentCombination,startFrom) => {
+    if (remainingSum === 0) {
+      //si devuelve cero la suma final, es valida nuestra combinacion
+      finalCombinations.push([...currentCombination])
+      return;
+    }
+
+    if (remainingSum < 0) {
+      //si devuelve valor negativo la suma final, retorna automaticamente
+      return;
+    }
+
+    //for que recorrera todos los numeros del array
+    for (let i = startFrom; i < candidates.length; i++) {
+      //variable para guardar el resto de la suma con el actual candidato
+      let rest = remainingSum - candidates[i];
+
+      //agrega el candidato al array de current combination
+      currentCombination.push(candidates[i]);
+
+      //ejecuta la funcion con la actualizacion de la suma final y el mismo inicio de array
+      combinationSumRecursive(candidates, rest, finalCombinations, currentCombination,i)
+
+      //elimina el candidato del array current combination
+      currentCombination.pop();
+    }
   }
   
   /**
@@ -19,7 +43,9 @@ const combinationSumRecursive = (
    * @return {number[][]}
    */
 const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
+    const finalCombinations = [];
+      combinationSumRecursive(candidates, target, finalCombinations, [], 0);
+      return finalCombinations;
   }
 
 module.exports = combinationSum;
