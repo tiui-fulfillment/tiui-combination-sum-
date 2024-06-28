05c2072418b4f8b4ecd76803c87251eb362acf38
obed <mendezobed98@gmail.com>
2024-06-28 15:07:46 -0600

    test combination sum

--- src/_test_/index.test.js
+++ src/_test_/index.test.js
@@ -1,9 +1,9 @@
-const combinationSum = require ('../index');
+const combinationSum = require('../index');
 
 describe('combinationSum', () => {
   it('Debería encontrar todas las combinaciones con suma específica', () => {
-    expect(combinationSum([1], 4)).toEqual([
-      [1, 1, 1, 1],
+    expect(combinationSum([1], 6)).toEqual([
+      [1, 1, 1, 1, 1, 1],
     ]);
 
     expect(combinationSum([2, 3, 6, 7], 7)).toEqual([
@@ -20,5 +20,7 @@ describe('combinationSum', () => {
     expect(combinationSum([2, 5], 3)).toEqual([]);
 
     expect(combinationSum([], 3)).toEqual([]);
+
+    expect(combinationSum([], 5)).toEqual([]);
   });
 });
