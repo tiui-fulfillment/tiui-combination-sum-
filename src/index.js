8818d512b9836278ce825335cc1a8624e86f40cf
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-06-28 20:37:48 -0600

    Problema Extra

--- src/index.js
+++ src/index.js
@@ -8,8 +8,36 @@
  */
 
 const combinationSumRecursive = (
-    
+  candidates,
+  remainingSum,
+  finalCombinations,
+  currentCombination,
+  startFrom
+) =>{
+  if(remainingSum === 0){//Si es 0 pasa currentCom a finalComb
+    finalCombinations.push([...currentCombination]);
+    return;
   }
+
+  if(remainingSum<0){//Si es menor a 0, retorna sin hacer nada
+    return;
+  }
+
+  for(let i= startFrom; i<candidates.length; i++){//igualamos startFrom en i para evitar duplicados
+    currentCombination.push(candidates[i]);
+    combinationSumRecursive(
+      candidates,
+      remainingSum-candidates[i],
+      finalCombinations,
+      currentCombination,
+      i
+    );
+    currentCombination.pop();
+  }
+
+
+}
+  
   
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
@@ -19,7 +47,9 @@ const combinationSumRecursive = (
    * @return {number[][]}
    */
 const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
+    const finalCombinations = [];
+    combinationSumRecursive(candidates,target,finalCombinations,[],0);
+    return finalCombinations;
   }
 
 module.exports = combinationSum;
