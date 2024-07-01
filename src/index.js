4a6e531220cea6545ff97084fdc51f903a08d868
Sahara <sahararod@gmail.com>
2024-07-01 11:56:43 -0600

    solucion a combination sum

--- src/index.js
+++ src/index.js
@@ -7,8 +7,22 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+const combinationSumRecursive = (candidates, remainingSum, finalCombinations, currentCombination, startFrom) => {
+    //If the combination is valid, the remianing sum is equal to zero
+    if(remainingSum === 0){
+      finalCombinations.push([...currentCombination]);
+      return;
+    }
+    //The exploration is ended if the remaining sum is negative
+    if(remainingSum < 0){
+      return;
+    }
+    //Explore each candidate from startFrom
+    for(let i = startFrom; i < candidates.length; i++){
+      currentCombination.push(candidates[i]); //Add the current combination
+      combinationSumRecursive(candidates, remainingSum - candidates[i], finalCombinations, currentCombination, i);
+      currentCombination.pop(); //Backtracking, eliminating the last candidate
+    }
   }
   
   /**
@@ -19,7 +33,9 @@ const combinationSumRecursive = (
    * @return {number[][]}
    */
 const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
+    const finalCombinations = [];
+    combinationSumRecursive(candidates, target, finalCombinations, [], 0);
+    return finalCombinations;
   }
 
 module.exports = combinationSum;
