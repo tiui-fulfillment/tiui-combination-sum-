fc2614e9b4f71121c3864321e878e34bfda3e673
Samir Priego <samir.apriego@gmail.com>
2024-07-01 17:27:31 -0600

    2.0
    Eercicio Resuelto

--- src/index.js
+++ src/index.js
@@ -7,9 +7,28 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+function combinationSumRecursive(candidates=[], target=0) {
+  const result = [];
+
+  function backtrack(remaining=0, start=0, path=[]) {
+      if (remaining < 0) {
+      return;
+      }
+      if (remaining === 0) {
+      result.push([...path]);
+      return;
+      }
+
+      for (let i = start; i < candidates.length; i++) {
+      path.push(candidates[i]);
+      backtrack(remaining - candidates[i], i, path);
+      path.pop();
+      }
   }
+
+  backtrack(target, 0, []);
+  return result;
+}
   
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
