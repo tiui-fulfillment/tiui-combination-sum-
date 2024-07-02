8a05b93c99999e12e00cda61555ead3ab0a8f25d
Juan13Ju <juan13ubaldo@gmail.com>
2024-07-01 20:14:23 -0600

    Solucion al problema combination sum

--- src/index.js
+++ src/index.js
@@ -7,10 +7,30 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+const combinationSumRecursive = (candidates, remainingSum, finalCombinations, currentCombination, startFrom, target) => {
+
+  const backtrack = (startFrom, remainingSum) => {
+    // Primer caso base en el que llegamos a la suma deseada
+    if (remainingSum == 0){
+      finalCombinations.push(currentCombination.slice().reverse())
+      return
+    }
+    // Segundo caso base en el que la suma es mayor o llegamos al fin de los candidatos
+    if(remainingSum < 0 || startFrom < 0){
+      return
+    }
+    // Caso en el que pasamos al siguiente candidato
+    backtrack(startFrom-1, remainingSum)
+
+    // Caso en el que añadimos a la lista el candidato actual
+    currentCombination.push(candidates[startFrom])
+    backtrack(startFrom, remainingSum - candidates[startFrom])
+    currentCombination.pop()
   }
-  
+
+  backtrack(startFrom, remainingSum)
+  return finalCombinations
+} 
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
    *
@@ -18,8 +38,8 @@ const combinationSumRecursive = (
    * @param {number} target
    * @return {number[][]}
    */
-const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
+const combinationSum = (candidates,target) => {
+    return combinationSumRecursive(candidates, target, [], [], candidates.length - 1);
   }
 
 module.exports = combinationSum;
